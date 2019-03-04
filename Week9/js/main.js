451bccdc8ae1342b0eace53a25afd4095d4fea46
visualstudio <visualstudio@230-PC21>
2019-03-04 16:59:56 -0500

    passing parameters

--- Week9/js/main.js
+++ Week9/js/main.js
@@ -1,1 +1,13 @@
-﻿// Your code here!
+﻿let addnumbers = {
+    sum: 0,
+    starter: 1109,
+    stepdown: 11,
+    stopnumber: 119,
+    adder: function () {
+        for (starter = 1109; starter >= stopnumber; starter -= stepdown) {
+            sum = sum + starter;
+        }
+        return sum;
+    }
+
+};
